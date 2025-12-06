c6b7787458fa13997069d83da8a40b76fc6ee586
anna <annabyabe@gmail.com>
2025-12-06 06:26:46 -0300

    Attempt 2

--- e2e-tests/App.test.jsx
+++ e2e-tests/App.test.jsx
@@ -9,9 +9,20 @@ describe('Pokedex', () => {
 })
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  test('front page can be opened part 2', async ({ page }) => {
      await page.goto('http://localhost:8080/pokemon/charmander')
     await expect(page.getByText('charmander')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+})
+
+describe('Pokedex', () => {
+  test('front page can be opened part 2', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+
+    await page.getByRole('link', { name: 'pikachu' }).click();
+    await expect(page).toHaveURL('http://localhost:8080/pokemon/pikachu');
+    await expect(page.getByText('pikachu')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
