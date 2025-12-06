94f92cb0a0773546789863339d7d5c692b5d9cac
anna <annabyabe@gmail.com>
2025-12-06 06:21:00 -0300

    Exercise 11.9

---
+++ e2e-tests/App.test.jsx
@@ -0,0 +1,17 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+     await page.goto('http://localhost:8080/pokemon/ivysaur')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+})
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+     await page.goto('http://localhost:8080/pokemon/charmander')
+    await expect(page.getByText('charmander')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+})
