d1adeb2ceb47889ade0e5bdd416a2ac726c6b922
annabeatrizzz <annabyabe@gmail.com>
2025-12-08 17:54:15 -0300

    Attempt 3

--- e2e-tests/App.test.jsx
+++ e2e-tests/App.test.jsx
@@ -1,8 +1,8 @@
-const { test, describe, expect, beforeEach } = require('@playwright/test')
+const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-     await page.goto('http://localhost:8080/pokemon/ivysaur')
+    await page.goto('http://localhost:8080/pokemon/ivysaur')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
@@ -10,7 +10,7 @@ describe('Pokedex', () => {
 
 describe('Pokedex', () => {
   test('front page can be opened part 2', async ({ page }) => {
-     await page.goto('http://localhost:8080/pokemon/charmander')
+    await page.goto('http://localhost:8080/pokemon/charmander')
     await expect(page.getByText('charmander')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
@@ -20,8 +20,8 @@ describe('Pokedex', () => {
   test('pokemon page can be navigated to', async ({ page }) => {
     await page.goto('http://localhost:8080/')
 
-    await page.getByRole('link', { name: 'pikachu' }).click();
-    await expect(page).toHaveURL('http://localhost:8080/pokemon/pikachu');
+    await page.getByRole('link', { name: 'pikachu' }).click()
+    await expect(page).toHaveURL('http://localhost:8080/pokemon/pikachu')
     await expect(page.getByText('pikachu')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
